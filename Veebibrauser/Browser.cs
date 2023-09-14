8a6967390bd75cf6bfc89a94345befbda9ef7f55
fngpr <fngpr@FNGPR>
2023-09-14 21:59:12 +0300

    Add project files.

---
+++ Veebibrauser/Browser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veebibrauser
+{
+    public class Browser
+    {
+        private Stack<string> _back = new Stack<string>();
+        private Stack<string> _forward = new Stack<string>();
+        public List<string> _history = new List<string>();
+
+        private string _current;
+
+        public Browser()
+        {
+            GoTo("google.com");
+        }
+
+        public void GoTo(string url)
+        {
+            _back.Push(_current);
+            _current = url;
+            _forward.Clear();
+            _history.Add(url);
+        }
+
+        public void Back()
+        {
+            if (_back.Count > 0)
+            {
+                _forward.Push(_current);
+                _current = _back.Pop();
+                _history.Add(_current);
+            }
+        }
+
+        public void Forward()
+        {
+            if (_forward.Count > 0)
+            {
+                _back.Push(_current);
+                _current = _forward.Pop();
+                _history.Add(_current);
+            }
+        }
+
+        public string Current()
+        {
+            return _current;
+        }
+
+        public List<string> History()
+        {
+            return _history;
+        }
+    }
+}
---
+++ Veebibrauser/Prog.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veebibrauser
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Loome brauseri eksemplari.
+            Browser site = new Browser();
+
+            // Toome välja kasutajajuhised.
+            Console.WriteLine("näidata ajalugu   history\n" +
+                              "edasi             forward\n" +
+                              "tagasi            back\n" +
+                              "praegune          current");
+            Console.WriteLine("");
+
+            while (true)
+            {
+                Console.Write("G:\\Web Browser> ");
+                string link = Console.ReadLine();
+
+                // Kui kasutaja sisestab "history", näidatakse külastatud saitide ajalugu.
+                if (link == "history" || link == "h")
+                {
+                    Console.WriteLine("");
+                    for (int i = 0; i < site.History().Count; i++)
+                    {
+                        Console.WriteLine(site.History()[i]);
+                    }
+                    Console.WriteLine("");
+                }
+
+                // Kui kasutaja sisestab "forward", liigume edasi järgmisele lehele ajaloos.
+                else if (link == "forward" || link == "f")
+                {
+                    site.Forward();
+                    Console.WriteLine(site.Current());
+                }
+
+                // Kui kasutaja sisestab "back", naaseme ajaloo eelmisele lehele.
+
+                else if (link == "back" || link == "b")
+                {
+                    site.Back();
+                    Console.WriteLine(site.Current());
+                }
+
+                // Kui kasutaja sisestab "current", kuvatakse konsoolil tõeline leht.
+                else if (link == "current" || link == "c")
+                {
+                    Console.WriteLine(site.Current());
+                }
+
+                // Vastasel juhul liigume kasutaja sisestatud lingile.
+                else { site.GoTo(link); }
+            }
+        }
+    }
+
+}
